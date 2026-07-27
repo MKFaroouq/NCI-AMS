dfc92f754886ebd5b5a78dd5a027a9ab17b59e81
MKFaroouq <iprobazooka@gmail.com>
2026-07-27 13:21:11 +0300

    Clinic Schema

---
+++ BackEnd/models/Clinic.js
@@ -0,0 +1,21 @@
+const clinicSchema = new mongoose.Schema({
+
+    name: {
+        type:     String,
+        required: true,
+        trim:     true
+    },
+
+    quota: {
+        type:    Number,
+        default: 100
+    },
+
+    isActive: {
+        type:    Boolean,
+        default: true
+    }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Clinic', clinicSchema);
