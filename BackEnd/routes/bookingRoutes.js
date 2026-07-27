da9e859e49b0e658ba71fe20aef55a1ad6848cd2
MKFaroouq <iprobazooka@gmail.com>
2026-07-27 14:27:38 +0300

    Post / Admin Login

--- BackEnd/routes/bookingRoutes.js
+++ BackEnd/routes/bookingRoutes.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const router  = express.Router();
-const { adminLogin } = require('../controllers/bookingController');
+const express=require('express');
+const router=express.Router();
+const { adminLogin } = require('../Controllers/bookingController');
+
 
 // POST /api/auth/login
 router.post('/auth/login', adminLogin);
