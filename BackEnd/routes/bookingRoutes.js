5a8c4d468e92c41d39a6476b376dc86bd34d38f0
MKFaroouq <iprobazooka@gmail.com>
2026-07-28 12:55:37 +0300

    Post / Image "Multer"

--- BackEnd/routes/bookingRoutes.js
+++ BackEnd/routes/bookingRoutes.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const router=express.Router();
-const { adminLogin } = require('../Controllers/bookingController');
+const { adminLogin , createBooking } = require('../Controllers/bookingController');
+const { upload } = require('../middleware/upload');
 
 
 // POST /api/auth/login
 router.post('/auth/login', adminLogin);
 
+// POST /api/bookings
+// upload.single => only one file will be uploaded with the field name 'nationalIdImage'
+router.post('/bookings', upload.single('nationalIdImage'), createBooking);
 module.exports = router;
