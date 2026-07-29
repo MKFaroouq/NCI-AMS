9e9df5756a2ff282ad394416625da9fee46933b6
MKFaroouq <iprobazooka@gmail.com>
2026-07-29 10:01:58 +0300

    Post / Create bookings

--- BackEnd/routes/bookingRoutes.js
+++ BackEnd/routes/bookingRoutes.js
@@ -1,13 +1,12 @@
 const express=require('express');
 const router=express.Router();
-const { adminLogin , createBooking } = require('../Controllers/bookingController');
-const { upload } = require('../middleware/upload');
+const { createBooking } = require('../Controllers/bookingController');
 
 
-// POST /api/auth/login
-router.post('/auth/login', adminLogin);
 
 // POST /api/bookings
 // upload.single => only one file will be uploaded with the field name 'nationalIdImage'
-router.post('/bookings', upload.single('nationalIdImage'), createBooking);
+router.post('/bookings', createBooking);
+
+
 module.exports = router;
