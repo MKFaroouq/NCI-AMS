390e12ea0d5208f7674866631c0581c1fa65fe31
MKFaroouq <iprobazooka@gmail.com>
2026-08-01 13:14:43 +0300

    Delete / Booking

--- BackEnd/routes/bookingRoutes.js
+++ BackEnd/routes/bookingRoutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const auth = require('../middleware/auth');
 
-const { createBooking , getAllBookings , approveBooking , rejectBooking , exportBookings} = require('../controllers/bookingController');
+const { createBooking , getAllBookings , approveBooking , rejectBooking , exportBookings , exportAllBookings , deleteBooking} = require('../controllers/bookingController');
 
 
 // POST /api/bookings
@@ -18,7 +18,15 @@ router.patch('/bookings/:id/approve', auth, approveBooking);
 // PATCH /api/bookings/:id/reject
 router.patch('/bookings/:id/reject', auth, rejectBooking);
 
+// Export all bookings to excel file
+router.post('/bookings/export/all', auth, exportAllBookings)
+
 // Export the booking to excel file
 router.post('/bookings/export/:clinicId', auth, exportBookings)
+
+// DELETE /api/bookings/:id
+router.delete('/bookings/:id', auth, deleteBooking);
+
+
 
 module.exports = router;
